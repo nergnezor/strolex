813a803a667e666d0590c4d3123ee97982e6d545
Erik Rosengren <e@roseng.ren>
2018-09-03 19:18:02 +0200

    debugging

--- gui/include/gui/screen1_screen/Screen1View.hpp
+++ gui/include/gui/screen1_screen/Screen1View.hpp
@@ -1,33 +1,27 @@
 #ifndef SCREEN1_VIEW_HPP
 #define SCREEN1_VIEW_HPP
 
-#include <gui_generated/screen1_screen/Screen1ViewBase.hpp>
 #include <gui/screen1_screen/Screen1Presenter.hpp>
+#include <gui_generated/screen1_screen/Screen1ViewBase.hpp>
 #include <touchgfx/widgets/AnimationTextureMapper.hpp>
-#include <touchgfx/widgets/canvas/Circle.hpp>
 #include <touchgfx/widgets/TextAreaWithWildcard.hpp>
+#include <touchgfx/widgets/canvas/Circle.hpp>
 #include <touchgfx/widgets/canvas/PainterRGB888.hpp>
 
-class Box10x10 : public CanvasWidget
-{
-public:
-  virtual Rect getMinimalRect() const
-  {
-    return Rect(0, 0, 10, 10);
-  }
-  virtual bool drawCanvasWidget(const Rect &invalidatedArea) const
-  {
+class Box10x10 : public CanvasWidget {
+ public:
+  virtual Rect getMinimalRect() const { return Rect(0, 0, 10, 10); }
+  virtual bool drawCanvasWidget(const Rect &invalidatedArea) const {
     Canvas canvas(this, invalidatedArea);
     canvas.moveTo(5, 0);
     canvas.lineTo(10, 5);
     canvas.lineTo(5, 10);
     canvas.lineTo(0, 5);
-    return canvas.render(); // Shape is automatically closed
+    return canvas.render();  // Shape is automatically closed
   }
 };
-class Screen1View : public Screen1ViewBase
-{
-public:
+class Screen1View : public Screen1ViewBase {
+ public:
   Screen1View();
   virtual ~Screen1View() {}
   virtual void setupScreen();
@@ -36,39 +30,37 @@ public:
 
   // Declaring callback handler for Image
   void ImageClickHandler(const Image &b, const ClickEvent &e);
-  void animationEndedCallbackHandler(const touchgfx::AnimationTextureMapper &src);
-  void setupHand(TextureMapper &hand, const BitmapId bitmapId, int16_t clockRotationCenterX, int16_t clockRotationCenterY, int16_t rotationCenterX, int16_t rotationCenterY, int width, int height);
+  void animationEndedCallbackHandler(
+      const touchgfx::AnimationTextureMapper &src);
+  void setupTextureMapper(TextureMapper &hand, Image *image);
 
-protected:
+ protected:
   // Declaring callback type of box and clickEvent
-  AnimationTextureMapper humidity;
-  AnimationTextureMapper humidity2;
+  AnimationTextureMapper textureMapper1;
+  AnimationTextureMapper textureMapper2;
 
   Callback<Screen1View, const Image &, const ClickEvent &> ImageClickedCallback;
   /*
-     * Callback Handler Declarations
-     */
+   * Callback Handler Declarations
+   */
   // void buttonCallbackHandler(const touchgfx::AbstractButton& src);
 
   /*
-     * Callback Declarations
-     */
+   * Callback Declarations
+   */
   Callback<Screen1View, const AnimationTextureMapper &> animationEndedCallback;
   int tickCounter;
 
-private:
-  void Rotate(touchgfx::AnimationTextureMapper &src);
+ private:
+  void Rotate(touchgfx::AnimationTextureMapper &src, float endValue);
   Circle circle;
   TextAreaWithOneWildcard txt;
   Unicode::UnicodeChar txtBuffer[10];
   void updateTxt(int newValue);
   void demo(void);
-  void bezelShrink();
+  void resize(ScalableImage *scalableImage1, int speed, int size);
   void scrollText();
-
-  // Box10x10 box;
-  // PainterRGB888 myPainter;
-  // Canvas canvas;
+  void bezelRotate(AnimationTextureMapper &src, float endValue);
 };
 
-#endif // SCREEN1_VIEW_HPP
+#endif  // SCREEN1_VIEW_HPP
--- gui/src/screen1_screen/Screen1View.cpp
+++ gui/src/screen1_screen/Screen1View.cpp
@@ -1,154 +1,210 @@
-#include <gui/screen1_screen/Screen1View.hpp>
 #include <BitmapDatabase.hpp>
-#include <touchgfx/widgets/Image.hpp>
-#include <touchgfx/widgets/TextureMapper.hpp>
-#include <touchgfx/EasingEquations.hpp>
+#include <gui/screen1_screen/Screen1View.hpp>
 #include <texts/TextKeysAndLanguages.hpp>
 #include <touchgfx/Color.hpp>
+#include <touchgfx/EasingEquations.hpp>
+#include <touchgfx/widgets/Image.hpp>
+#include <touchgfx/widgets/TextureMapper.hpp>
 
 #define D 390
 #define CANVAS_BUFFER_SIZE (3600)
-// // Setup the CanvasWidgetRenderer. ONLY needed if you use CanvasWidgets
-// // in your application. The CANVAS_BUFFER_SIZE can be adjusted to match
-// // your needs in performance vs. RAM usage.
-// static uint8_t canvasBuffer[CANVAS_BUFFER_SIZE];
-// CanvasWidgetRenderer::setupBuffer(canvasBuffer, CANVAS_BUFFER_SIZE);
-
-Screen1View::Screen1View() : ImageClickedCallback(this, &Screen1View::ImageClickHandler),
-                             animationEndedCallback(this, &Screen1View::animationEndedCallbackHandler)
-
-{
-}
-
-/*
-  * Creates a gauge hand using a texture mapper
-  */
-void Screen1View::setupHand(TextureMapper &hand, const BitmapId bitmapId, int16_t clockRotationCenterX, int16_t clockRotationCenterY, int16_t rotationCenterX, int16_t rotationCenterY, int width, int height)
-{
-    hand.setBitmap(Bitmap(bitmapId));
-    hand.setWidth(width);
-    hand.setHeight(height);
-    // hand.setWidth(D);
-    // hand.setHeight(D);
-    hand.setXY(0, 0);
-    hand.setBitmapPosition(clockRotationCenterX - rotationCenterX, clockRotationCenterY - rotationCenterY);
-    hand.setCameraDistance(300.0f);
-    hand.setOrigo((float)clockRotationCenterX, (float)clockRotationCenterY, hand.getCameraDistance());
-    hand.setCamera(hand.getOrigoX(), hand.getOrigoY());
-    hand.setRenderingAlgorithm(TextureMapper::BILINEAR_INTERPOLATION);
-    hand.setVisible(true);
-}
-
-void Screen1View::setupScreen()
-{
-    Image *image = &rolex_arrow1;
-    rolex_urtavla.setClickAction(ImageClickedCallback);
-    int16_t width = image->getWidth();
-    int16_t height = image->getHeight();
-    setupHand(humidity, image->getBitmap(), D / 2, D / 2, width / 2, D / 2, width, height); //Placing the rotation centor of humidity hand at (103, 195)
-    humidity.setTextureMapperAnimationEndedAction(animationEndedCallback);
-    remove(*image);
-    add(humidity);
-
-    bgBox.setColor(Color::getColorFrom24BitRGB(0, 0, 0));
-
-    textArea1.setX(D);
-    textArea2.setX(D);
-}
-
-void Screen1View::updateTxt(int newValue)
-{
-    Unicode::snprintf(txtBuffer, 10, "%d", newValue);
-    txt.invalidate();
-}
-
-void Screen1View::tearDownScreen()
-{
-}
 
 static float angle = 0.0f;
 static volatile bool isAnimating = false;
-static volatile bool looping = false;
-void Screen1View::Rotate(touchgfx::AnimationTextureMapper &src)
-{
-    if (!isAnimating)
-    {
-        angle += 2;
-        // src.setupAnimation(AnimationTextureMapper::Z_ROTATION, angle, 10, 0, EasingEquations::cubicEaseInOut);
-        src.setupAnimation(AnimationTextureMapper::SCALE, src.getScale() * 0.9, 20, 0);
-        isAnimating = true;
+static volatile bool looping = true;
+static int demoI = 0;
+
+Screen1View::Screen1View()
+    : ImageClickedCallback(this, &Screen1View::ImageClickHandler),
+      animationEndedCallback(this, &Screen1View::animationEndedCallbackHandler)
+
+{}
+
+/*
+ * Creates a gauge textureMapper using a texture mapper
+ */
+// void Screen1View::setupTextureMapper(TextureMapper& textureMapper, const
+// BitmapId bitmapId,
+//                             int16_t clockRotationCenterX,
+//                             int16_t clockRotationCenterY,
+//                             int16_t rotationCenterX, int16_t rotationCenterY,
+//                             int width, int height) {
+void Screen1View::setupTextureMapper(TextureMapper& textureMapper,
+                                     Image* image) {
+  //   Image* image = &rolex1_ring_3901;
+  int16_t width = image->getWidth();
+  int16_t height = image->getHeight();
+  textureMapper.setBitmap(Bitmap(image->getBitmap()));
+  textureMapper.setWidth(width);
+  textureMapper.setHeight(height);
+  //   textureMapper.setWidth(D);
+  //   textureMapper.setHeight(D);
+  textureMapper.setXY(0, 0);
+  textureMapper.setBitmapPosition(D / 2 - width / 2, D / 2 - D / 2);
+  textureMapper.setCameraDistance(300.0f);
+  textureMapper.setOrigo((float)D / 2, (float)D / 2,
+                         textureMapper.getCameraDistance());
+  textureMapper.setCamera(textureMapper.getOrigoX(), textureMapper.getOrigoY());
+  textureMapper.setRenderingAlgorithm(TextureMapper::BILINEAR_INTERPOLATION);
+  //   textureMapper.setVisible(true);
+  remove(*image);
+}
+
+void Screen1View::setupScreen() {
+  rolex_urtavla.setClickAction(ImageClickedCallback);
+
+  setupTextureMapper(textureMapper1, &rolex1_ring_3901);
+  textureMapper1.setTextureMapperAnimationEndedAction(animationEndedCallback);
+  textureMapper1.setVisible(true);
+  add(textureMapper1);
+  //   setupTextureMapper(textureMapper2, &rolex_arrow1);
+  //   textureMapper2.setTextureMapperAnimationEndedAction(animationEndedCallback);
+
+  remove(rolex_urtavla);
+  add(rolex_urtavla);
+
+  bgBox.setColor(Color::getColorFrom24BitRGB(0, 0, 0));
+
+  textArea1.setX(D);
+  textArea2.setX(D);
+}
+
+void Screen1View::updateTxt(int newValue) {
+  Unicode::snprintf(txtBuffer, 10, "%rect.width", newValue);
+  txt.invalidate();
+}
+
+void Screen1View::tearDownScreen() {}
+
+void Screen1View::ImageClickHandler(const Image& b, const ClickEvent& evt) {
+  if (&b == &rolex_urtavla &&
+      (&evt)->getType() == ClickEvent::ClickEventType::PRESSED) {
+    // looping = !looping;
+    ++demoI;
+    demo();
+  }
+}
+void Screen1View::handleTickEvent() {
+  if (looping) {
+    demo();
+  }
+}
+void Screen1View::Rotate(touchgfx::AnimationTextureMapper& src,
+                         float endValue) {
+  isAnimating = true;
+  // if (!isAnimating)
+  {
+    angle = angle == 0 ? endValue : 0;
+
+    src.setupAnimation(AnimationTextureMapper::Z_ROTATION, angle, 4, 0,
+                       //    EasingEquations::cubicEaseInOut);
+                       EasingEquations::linearEaseNone);
+    // src.setupAnimation(AnimationTextureMapper::SCALE,
+    //                    src.getScale() * (D - 80) / D, 20, 0);
+    // if (src.getScale() == 1)
+    //   src.setupAnimation(AnimationTextureMapper::SCALE, 0.7f, 10, 0);
+    // else
+    //   src.setupAnimation(AnimationTextureMapper::SCALE, 1.0f, 5, 0);
+    src.startAnimation();
+  }
+}
+
+void Screen1View::animationEndedCallbackHandler(
+    const touchgfx::AnimationTextureMapper& src) {
+  static bool shrank = false;
+  if (&src == &textureMapper1) {
+    isAnimating = false;
+
+    if (!shrank) {
+      shrank = true;
+      Rotate(textureMapper1, 0);
+    } else {
+      shrank = false;
+      textureMapper1.setVisible(false);
+      scalableImage1.setVisible(true);
+      textureMapper1.invalidate();
+      ++demoI;
     }
-}
-static int demoI = -1;
+  }
+  //   if (&src == &textureMapper2) {
+  //     isAnimating = false;
 
-void Screen1View::ImageClickHandler(const Image &b, const ClickEvent &evt)
-{
-    if (&b == &rolex_urtavla && (&evt)->getType() == ClickEvent::ClickEventType::PRESSED)
-    {
-        looping = !looping;
-        ++demoI;
-        demoI = demoI % 4;
-    }
-}
-
-void Screen1View::animationEndedCallbackHandler(const touchgfx::AnimationTextureMapper &src)
-{
-    if (&src == &humidity)
-    {
-        isAnimating = false;
-        if (looping)
-            Rotate(humidity);
-    }
+  //     if (!shrank) {
+  //       shrank = true;
+  //       Rotate(textureMapper1, 0);
+  //     } else {
+  //       shrank = false;
+  //       textureMapper1.setVisible(false);
+  //       scalableImage1.setVisible(true);
+  //       scalableImage1.invalidate();
+  //       ++demoI;
+  //     }
+  //   }
 }
 
-void Screen1View::bezelShrink()
-{
-    static int dd = -8;
-    int d = scalableImage1.getWidth();
-    d += dd;
-    // if (d > D)
-    //     d = D;
-
-    d = MIN(D, d);
-    scalableImage1.setWidth(d);
-    scalableImage1.setHeight(d);
-    scalableImage1.setPosition(scalableImage1.getX() - dd / 2, scalableImage1.getY() - dd / 2, d, d);
-    bgBox.invalidate();
-    if (d < (D - 80) || d >= D)
-        dd *= -1;
+void Screen1View::bezelRotate(AnimationTextureMapper& src, float endValue) {
+  if (!isAnimating) {
+    isAnimating = true;
+    scalableImage1.setVisible(false);
+    src.setVisible(true);
+    Rotate(src, endValue);
+  }
 }
 
-void Screen1View::scrollText()
-{
-    int text_x = textArea1.getX();
-    if (text_x <= -D)
-    {
-        text_x = D;
-    }
-    textArea1.moveTo(text_x - 3, 0);
-    textArea2.moveTo(text_x, textArea1.getY());
+void Screen1View::resize(ScalableImage* src, int speed, int targetSize) {
+  //   int rect.width = src->getWidth();
+  Rect rect = src->getAbsoluteRect();
+  int dd = (1 << speed) * (rect.width > targetSize ? -1 : 1);
+  rect.width += dd;
+  //   rect.y - dd / 2;
+  //   if (abs(rect.width + dd - targetSize) < abs(dd)) {
+  if ((rect.width >= targetSize && dd > 0) ||
+      (rect.width <= targetSize && dd < 0)) {
+    rect.width = targetSize;
+    // dd = (targetSize - rect.width) / 2;
+    ++demoI;
+  }
+  //   rect.x -= dd / 2;
+  src->setWidth(rect.width);
+  src->setHeight(rect.width);
+  int x = (D - rect.width) / 2;
+  src->setPosition(x, x, rect.width, rect.width);
+  bgBox.invalidate();
 }
 
-void Screen1View::handleTickEvent()
-{
-
-    if (looping)
-    {
-        demo();
-    }
+void Screen1View::scrollText() {
+  int text_x = textArea1.getX();
+  if (text_x <= -textArea1.getWidth()) {
+    ++demoI;
+    text_x = D;
+  }
+  textArea1.moveTo(text_x - 5, 0);
+  textArea2.moveTo(text_x, textArea1.getY());
 }
 
-void Screen1View::demo()
-{
-    switch (demoI)
-    {
+void Screen1View::demo() {
+  demoI = demoI % 6;
+  switch (demoI) {
     case 0:
-        bezelShrink();
-        break;
+      bezelRotate(textureMapper1, 0.01f);
+      break;
+    case 1:
+      resize(&scalableImage1, 2, D - 80);
+      break;
     case 2:
-        scrollText();
-        break;
-    }
-
-    // ++demoI;
+      resize(&scalableImage1, 2, D);
+      break;
+    case 3:
+      resize(&scalableImage1, 4, D - 80);
+      break;
+    case 4:
+      scrollText();
+      break;
+    case 5:
+      bezelRotate(textureMapper2, 0.1f);
+      break;
+    case 6:
+      resize(&scalableImage1, 4, D - 80);
+      break;
+  }
 }
